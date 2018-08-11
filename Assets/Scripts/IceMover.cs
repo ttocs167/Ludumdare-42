dc938c08b914ba08806a4356ae7c729e61283b99
Rob <carvster.rs@gmail.com>
2018-08-11 20:29:59 +0100

    Texturing and backdrop prefab

---
+++ Assets/Scripts/IceMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceMover : MonoBehaviour {
+
+	private Vector2 uvOffset = Vector2.zero;
+	public Vector2 uvAnimationRate = new Vector2(0.0f, 1.0f);
+	int materialIndex = 0;
+	string textureName = "_MainTex";
+	private Renderer renderer;
+
+	void Start() {
+		renderer = GetComponent<Renderer>();
+	}
+
+	void Update () {
+		uvOffset += ( uvAnimationRate * Time.deltaTime );
+		if(renderer.enabled)
+		{
+			renderer.materials[materialIndex].SetTextureOffset(textureName, uvOffset );
+		}
+	}
+}
